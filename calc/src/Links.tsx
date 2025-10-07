5d7b85a58e5199c0e47634cd21845e0318c0ccd3
aveSova <tntseva02@gmail.com>
2025-10-07 09:14:49 +0300

    i'm tired

--- calc/src/Links.tsx
+++ calc/src/Links.tsx
@@ -9,8 +9,11 @@ const Links: React.FC = () => (
                 components: {
                 Button: {
                     fontFamily: 'Ubuntu',
+                    colorText: 'white',
+                    colorTextHeading: 'white',
+                    colorBgContainer: '#00296b',
                 }
-                }
+                },
             }}>
         </ConfigProvider>
         <Flex vertical justify='center'>
@@ -20,7 +23,7 @@ const Links: React.FC = () => (
             <Button type='text' href='https://kubsu.ru/index.html'>
                 абсолютную гиперссылку на главную страницу сайта kubsu.ru в протоколе https
             </Button>
-            <Image src='/src/assets/Prikol.png' alt="лого КубГУ (ссылка)" preview={false} 
+            <Image src='./src/assets/Prikol.png' alt="лого КубГУ (ссылка)" preview={false} 
             style={{ cursor: 'pointer' }}
             onClick={() => window.open('/target-page', '_blank')}/>
             <Button type='text' href='https://clck.ru/3PDHiw'>
@@ -41,16 +44,19 @@ const Links: React.FC = () => (
             <Button type='text' href='/index.html'>
                 относительную на страницу в текущем каталоге
             </Button>
-            <Button type='text' href='/src/about/task.html'>
+            <Button type='text' href='/about/task.html'>
                 относительную на страницу в каталоге about
             </Button>
-            <Button type='text' href='/src/about/task12/task12.html'>
+            <Button type='text' href='/about/task12/task12.html'>
                 здесь 12-й пункт атвичяю
             </Button>
-            <Typography style={{color:'white'}}>В этом тексте спрятана <Button type='text' href='https://png.klev.club/uploads/posts/2024-05/png-klev-club-0bci-p-molodets-png-27.png' style={{width:'100px'}}>ссылка</Button>, найдите её :)</Typography>
+            <Button type='text' href='/about/task12/task12.html'>
+                здесь 12-й пункт атвичяю
+            </Button>
+            <Typography>В этом тексте спрятана <Button type='text' href='https://png.klev.club/uploads/posts/2024-05/png-klev-club-0bci-p-molodets-png-27.png'>ссылка</Button>, найдите её :)</Typography>
             <Button type='text' href='https://skillbox.ru/media/code/teg-a-v-html-kak-delat-ssylki-na-stranicy-fayly-i-razdely/#stk-5'>ссылку на фрагмент страницы стороннего сайта</Button>
             <map name="task15" style={{width: '100%', maxWidth: '300px'}}>
-                <img src="/src/assets/ForTask.png" alt="задание 15" style={{width: '100%', maxWidth: '300px'}} />
+                <img src="./src/assets/ForTask.png" alt="задание 15" style={{width: '100%', maxWidth: '300px'}} />
                 <area shape="circle" coords="98,115,50" 
                     href="https://developer.mozilla.org/ru/docs/Web/HTML/Reference/Elements/map#живой_пример" alt="круглый идиот" />
                 <area shape="rect" coords="99,245,268,344" 
