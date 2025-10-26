8ac5251e129999934dd7c8617cdf49b55f40fdfb
aveSova <tntseva02@gmail.com>
2025-10-26 14:51:48 +0300

    oops

--- gallery/src/App.jsx
+++ gallery/src/App.jsx
@@ -17,6 +17,17 @@ function App() {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
+  const images = [
+    { src: '/gal/public/arbuz.webp', alt: 'Арбуз' },
+    { src: '/gal/public/dynia.webp', alt: 'Тыква' },
+    { src: '/gal/public/tykva.webp', alt: 'Дыня' },
+    { src: '/gal/public/granat.webp' , alt: 'Гранат' },
+    { src: '/gal/public/pomelo.webp' , alt: 'Помело' },
+    { src: '/gal/public/orange.webp' , alt: 'Апельсин' },
+    { src: '/gal/public/durian.webp' , alt: 'Дурь' },
+    { src: '/gal/public/pineapple.webp' , alt: 'Ананас' },
+  ];
+
   return (
     <>
     <ConfigProvider
@@ -29,19 +40,20 @@ function App() {
       <Carousel 
       arrows={true}
       infinite={true}
-      dots={false}
+      dots={true}
       slidesToShow={slidesToShow}
       slidesToScroll={1}
       centerMode={true}
       centerPadding="0"
-      focusOnSelect={true}
-      style={{
-            width: "100%",
-            margin: "0 auto"
-          }}>
-          <div><img  src='/gal/public/arbuz.webp'/></div>
-          <div><img  src='/gal/public/dynia.webp'/></div>
-          <div><img  src='/gal/public/tykva.webp'/></div>
+      focusOnSelect={true}>
+          {images.map((image, index) => (
+          <div key={index}>
+          <img 
+            src={image.src} 
+            alt={image.alt}
+            title={image.title}
+          /></div>
+          ))}
       </Carousel>
     </ConfigProvider>
     </>
