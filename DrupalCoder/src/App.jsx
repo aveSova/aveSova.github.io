61866ba9e5cebfed19a579a8193911be78000bfa
aveSova <tntseva02@gmail.com>
2025-12-16 10:12:30 +0300

    починил импорты наших компонентов в App.jsx

--- DrupalCoder/src/App.jsx
+++ DrupalCoder/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import Masha from './Masha'
-import Sasha from './Sasha'
-import Seva from './Seva'
+import Masha from './Masha/Masha'
+import Sasha from './Sasha/Sasha'
+import Seva from './Seva/Seva'
 function App() {
   const [count, setCount] = useState(0)
 
